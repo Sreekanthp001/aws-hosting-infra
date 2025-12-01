954f20ac74431f8b958bf00189e8c4d029ca5e2f
sreekanthp <varshisree2@gmail.com>
2025-12-01 17:51:39 +0530

    infra creation

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -1,2 +1,7 @@
-output "cluster_name" { value = aws_ecs_cluster.this.name }
-output "services" { value = { for k, v in aws_ecs_service.svc : k => v.name } }
+output "cluster_name" {
+  value = aws_ecs_cluster.this.name
+}
+
+output "services" {
+  value = { for k, v in aws_ecs_service.svc : k => v.name }
+}
--- variables.tf
+++ variables.tf
@@ -1,72 +1,31 @@
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
+variable "ecs_cluster_name" {
+  type = string
 }
 
-variable "aws_account_id" {
-  type    = string
-  default = "535462128585"
+variable "vpc_id" {
+  type = string
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
 }
 
 variable "environment" {
-  type    = string
-  default = "prod"
+  type = string
 }
 
-variable "domain" {
-  description = "Primary domain name (example: sree84s.site)"
-  type        = string
-  default     = "sree84s.site"
+variable "aws_region" {
+  type = string
 }
 
-variable "hosted_zone_id" {
-  description = "Route53 Hosted Zone ID for the domain. If empty, Terraform will look it up."
-  type        = string
-  default     = "Z0602795P0OBBBRHSRWB"
+variable "aws_account_id" {
+  type = string
 }
 
-variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
-}
-
-variable "public_subnet_count" {
-  type    = number
-  default = 2
-}
-
-variable "private_subnet_count" {
-  type    = number
-  default = 2
-}
-
-variable "azs" {
-  type    = list(string)
-  default = []
-}
-
-variable "ecs_cluster_name" {
-  type    = string
-  default = "sree-ecs-cluster"
-}
-
-
-variable "tfstate_s3_bucket" {
-  type    = string
-  default = ""
-}
-
-variable "tfstate_s3_key" {
-  type    = string
-  default = "terraform/state.tfstate"
-}
-
-variable "tfstate_lock_table" {
-  type    = string
-  default = ""
-}
-
-variable "ses_region" {
-  type    = string
-  default = "us-east-1"
+variable "services" {
+  description = "Map of ECS services with image and port"
+  type = map(object({
+    image = string
+    port  = number
+  }))
 }
