0bfd8e8f9bc04f7f7079391c8b3960a39922e7f5
sreekanthp <varshisree2@gmail.com>
2025-12-01 23:30:30 +0530

    infra creation

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -84,12 +84,13 @@ resource "aws_lb_listener" "https" {
 }
 
 resource "aws_lb_target_group" "tg" {
-  for_each  = toset(var.services)
+  for_each = toset(var.services)
 
-  name     = "${each.key}-${replace(var.domain, ".", "-")}"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  name        = "${each.key}-${replace(var.domain, ".", "-")}"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
 
   health_check {
     path     = "/"
