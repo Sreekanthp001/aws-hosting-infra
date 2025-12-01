79c86ab3659b036ecdda6962c4eb76e32e8c5b54
sreekanthp <varshisree2@gmail.com>
2025-12-01 08:26:46 +0530

    infra creation

--- main.tf
+++ main.tf
@@ -73,12 +73,22 @@ resource "aws_security_group" "alb_sg" {
   description = "allow HTTP/HTTPS"
   vpc_id = aws_vpc.this.id
   ingress {
-    protocol = "tcp" from_port = 80 to_port = 80 cidr_blocks = ["0.0.0.0/0"]
+    protocol = "tcp" 
+    from_port = 80 
+    to_port = 80 
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    protocol = "tcp" from_port = 443 to_port = 443 cidr_blocks = ["0.0.0.0/0"]
+    protocol = "tcp" 
+    from_port = 443 
+    to_port = 443 
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  egress { from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"] }
+  egress { 
+    from_port = 0 
+    to_port = 0 
+    protocol = "-1" 
+    cidr_blocks = ["0.0.0.0/0"] }
 }
 
 # Target group
