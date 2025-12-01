d180acb0493e5f39ea851d65c2aafde6f381eb32
sreekanthp <varshisree2@gmail.com>
2025-12-01 20:34:44 +0530

    infra creation

--- modules/ses/main.tf
+++ modules/ses/main.tf
@@ -17,9 +17,21 @@ resource "aws_route53_record" "verification" {
   records = [aws_ses_domain_identity.this.verification_token]
 }
 
+resource "aws_route53_record" "root_alb" {
+  zone_id = "Z01202283P3B2XPK9DBXI"
+  name    = "venturemond.site"
+  type    = "A"
+  alias {
+    name                   = module.alb.dns_name
+    zone_id                = module.alb.zone_id
+    evaluate_target_health = false
+  }
+}
+
 # Route53 CNAME records for DKIM
 # AWS always returns 3 DKIM tokens, so we can create 3 records safely
 resource "aws_route53_record" "dkim" {
+  allow_overwrite = true
   count   = 3
   zone_id = var.hosted_zone_id
   name    = "${aws_ses_domain_dkim.this.dkim_tokens[count.index]}._domainkey.${var.domain}"
