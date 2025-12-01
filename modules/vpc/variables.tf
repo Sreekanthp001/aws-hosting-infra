b8ff63e973b1f11111b1defb9b188291dc19d215
sreekanthp <varshisree2@gmail.com>
2025-12-01 08:50:37 +0530

    infra creation

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -2,3 +2,7 @@ variable "name" { type = string }
 variable "cidr" { type = string }
 variable "public_azs" { type = list(string) }
 variable "private_azs" { type = list(string) }
+variable "nat_azs" { 
+    type = list(string) 
+    default = [] 
+}
