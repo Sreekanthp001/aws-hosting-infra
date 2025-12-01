4fe29e781e6f7e8a93a05316540d55f8549e55c1
sreekanthp <varshisree2@gmail.com>
2025-12-01 11:33:55 +0530

    infra creation

--- main.tf
+++ main.tf
@@ -136,6 +136,8 @@ resource "aws_lb_target_group" "tg" {
   port = 80
   protocol = "HTTP"
   vpc_id = module.vpc.vpc_id
+  target_type = "tp"
+  
   health_check {
     path = "/"
     matcher = "200-399"
