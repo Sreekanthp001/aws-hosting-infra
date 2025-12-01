ba5f79bb7952f2e15bb77faa0d43fef03beaf4b2
sreekanthp <varshisree2@gmail.com>
2025-12-01 16:47:46 +0530

    infra creation

--- modules/ses/main.tf
+++ modules/ses/main.tf
@@ -1,8 +1,4 @@
-module "ses" {
-  source         = "./modules/ses"
-  domain         = var.domain
-  hosted_zone_id = module.route53.zone_id
-}
+# SES Domain Identity
 resource "aws_ses_domain_identity" "this" {
   domain = var.domain
 }
@@ -22,13 +18,11 @@ resource "aws_route53_record" "verification" {
 }
 
 # Route53 CNAME records for DKIM
-# NOTE: Must apply in two steps because dkim_tokens are unknown until apply
 resource "aws_route53_record" "dkim" {
   for_each = { for t in aws_ses_domain_dkim.this.dkim_tokens : t => t }
-
-  zone_id = var.hosted_zone_id
-  name    = "${each.value}._domainkey.${var.domain}"
-  type    = "CNAME"
-  ttl     = 600
-  records = ["${each.value}.dkim.amazonses.com"]
+  zone_id  = var.hosted_zone_id
+  name     = "${each.value}._domainkey.${var.domain}"
+  type     = "CNAME"
+  ttl      = 600
+  records  = ["${each.value}.dkim.amazonses.com"]
 }
