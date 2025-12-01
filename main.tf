a34eb173669625937822ff3f3ba31b40909789e2
sreekanthp <varshisree2@gmail.com>
2025-12-01 21:17:10 +0530

    infra creation

--- main.tf
+++ main.tf
@@ -51,6 +51,7 @@ module "alb" {
   source             = "./modules/alb"
   vpc_id             = module.vpc.vpc_id
   subnets         = module.vpc.public_subnets
+  security_group_id = aws_security_group.alb_sg.id
   domain             = var.domain
   hosted_zone_id     = var.hosted_zone_id
   aws_region         = var.aws_region
