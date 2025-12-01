fd325a87ba57d2ea05c2b2edbdfe09cbaeb5bc89
sreekanthp <varshisree2@gmail.com>
2025-12-01 20:04:52 +0530

    infra creation

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -6,30 +6,30 @@ data "aws_route53_zone" "selected" {
 
 # ALIAS for root and www to ALB
 resource "aws_route53_record" "root_alb" {
-  zone_id = data.aws_route53_zone.selected.zone_id
+  zone_id = "Z0602795P0OBBBRHSRWB"
   name = var.domain
   type = "A"
   alias {
     name = var.alb_dns_name
-    zone_id = data.aws_route53_zone.selected.zone_id # ALIAS expects ALB zone id; Terraform may need correct zone id. Using simple approach; you may adjust.
+    zone_id = "Z0602795P0OBBBRHSRWB"
     evaluate_target_health = true
   }
 }
 
 resource "aws_route53_record" "www_alb" {
-  zone_id = data.aws_route53_zone.selected.zone_id
+  zone_id = "Z0602795P0OBBBRHSRWB"
   name = "www.${var.domain}"
   type = "A"
   alias {
     name = var.alb_dns_name
-    zone_id = data.aws_route53_zone.selected.zone_id
+    zone_id = "Z0602795P0OBBBRHSRWB"
     evaluate_target_health = true
   }
 }
 
 # static asset record
 resource "aws_route53_record" "static" {
-  zone_id = data.aws_route53_zone.selected.zone_id
+  zone_id = "Z0602795P0OBBBRHSRWB"
   name = "static.${var.domain}"
   type = "CNAME"
   ttl = 300
