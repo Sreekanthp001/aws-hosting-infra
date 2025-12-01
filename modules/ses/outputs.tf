85ae0bb672d45ddf3191da7e1c492397a244518f
sreekanthp <varshisree2@gmail.com>
2025-12-01 16:50:47 +0530

    infra creation

--- modules/ses/outputs.tf
+++ modules/ses/outputs.tf
@@ -1,7 +1,14 @@
+# SES Domain Identity ARN
 output "domain_identity_arn" {
   value = aws_ses_domain_identity.this.arn
 }
 
+# SES Domain verification token
+output "domain_verification_token" {
+  value = aws_ses_domain_identity.this.verification_token
+}
+
+# SES DKIM tokens
 output "dkim_tokens" {
   value = aws_ses_domain_dkim.this.dkim_tokens
 }
