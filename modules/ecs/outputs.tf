bad8cf1604b220671574dcfce7f747c8c7797665
sreekanthp <varshisree2@gmail.com>
2025-12-03 17:44:43 +0530

    infra creation

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -1,7 +1,14 @@
-output "cluster_name" {
-  value = aws_ecs_cluster.this.name
+output "service_names" {
+  description = "ECS Service Names"
+  value       = { for k, v in aws_ecs_service.svc : k => v.name }
 }
 
-output "service_names" {
-  value = { for k, v in aws_ecs_service.svc : k => v.name }
+output "service_arns" {
+  description = "ECS Service ARNs"
+  value       = { for k, v in aws_ecs_service.svc : k => v.arn }
+}
+
+output "task_definition_arns" {
+  description = "Task Definition ARNs"
+  value       = { for k, v in aws_ecs_task_definition.task : k => v.arn }
 }
