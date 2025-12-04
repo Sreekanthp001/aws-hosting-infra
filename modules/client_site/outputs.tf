f127aadccfe461126fc110beb8211ae7125b7f38
Sreekanthp001 <sreekanthpaleti1999@gmail.com>
2025-12-04 11:24:59 +0530

    infra creation

--- modules/client_site/outputs.tf
+++ modules/client_site/outputs.tf
@@ -1,33 +1,19 @@
-output "domain" {
-  value = var.domain
-}
-
-output "certificate_arn" {
-  value       = try(aws_acm_certificate.cert[0].arn, null)
-  description = "ACM certificate for this domain"
-}
-
 output "ses_identity_arn" {
-  value       = try(aws_ses_domain_identity.domain_identity.arn, null)
-  description = "SES domain identity ARN"
+  value = var.enable_ses ? aws_ses_domain_identity.domain_identity[0].arn : null
 }
 
 output "ecs_service_name" {
-  value       = try(aws_ecs_service.client_service.name, null)
-  description = "ECS service created for this client"
+  value = var.create_ecs ? aws_ecs_service.client_service[0].name : null
 }
 
 output "target_group_arn" {
-  value       = try(aws_lb_target_group.client_tg.arn, null)
-  description = "Target group ARN for host-based routing"
+  value = var.create_ecs ? aws_lb_target_group.client_tg[0].arn : null
 }
 
 output "listener_rule_arn" {
-  value       = try(aws_lb_listener_rule.client_rule.arn, null)
-  description = "Listener rule that routes domain to ECS service"
+  value = var.create_ecs ? aws_lb_listener_rule.client_rule[0].arn : null
 }
 
 output "cloudfront_distribution_id" {
-  value       = try(aws_cloudfront_distribution.client_cf.id, null)
-  description = "CloudFront distribution ID for static site"
+  value = var.create_cloudfront ? aws_cloudfront_distribution.client_cf[0].id : null
 }
