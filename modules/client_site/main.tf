e190d0667e33318a4b09043fff6e43734d403be2
Sreekanthp001 <sreekanthpaleti1999@gmail.com>
2025-12-04 12:49:07 +0530

    infra creation

--- modules/client_site/main.tf
+++ modules/client_site/main.tf
@@ -6,24 +6,3 @@ resource "aws_acm_certificate" "cert" {
   subject_alternative_names = ["www.${var.domain}"]
 }
 
-# create Route53 records for cert validation (need domain zone)
-resource "aws_route53_record" "cert_validation" {
-  for_each = (
-    length(aws_acm_certificate.cert) > 0
-    ? {
-        for dvo in aws_acm_certificate.cert[0].domain_validation_options :
-        dvo.domain_name => {
-          name  = dvo.resource_record_name
-          type  = dvo.resource_record_type
-          value = dvo.resource_record_value
-        }
-      }
-    : {}
-  )
-
-  zone_id = var.hosted_zone_id
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.value]
-  ttl     = 60
-}
