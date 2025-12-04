55aad72f639e90cc1bf9f67c060f9777cc5d5f14
Sreekanthp001 <sreekanthpaleti1999@gmail.com>
2025-12-04 13:26:22 +0530

    infra creation

---
+++ modules/monitoring/main.tf
@@ -0,0 +1,42 @@
+resource "aws_sns_topic" "alerts" {
+  name = "${var.project}-alerts"
+}
+
+resource "aws_sns_topic_subscription" "email" {
+  count = var.sns_alert_email != "" ? 1 : 0
+  topic_arn = aws_sns_topic.alerts.arn
+  protocol  = "email"
+  endpoint  = var.sns_alert_email
+}
+
+resource "aws_cloudwatch_metric_alarm" "alb_5xx" {
+  alarm_name          = "${var.project}-alb-5xx"
+  namespace           = "AWS/ApplicationELB"
+  metric_name         = "HTTPCode_ELB_5XX_Count"
+  statistic           = "Sum"
+  dimensions = { LoadBalancer = var.alb_name }
+  period              = 300
+  evaluation_periods  = 1
+  threshold           = 5
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "ecs_tasks_below" {
+  alarm_name = "${var.project}-ecs-tasks-below"
+  namespace  = "AWS/ECS"
+  metric_name = "RunningTaskCount"
+  dimensions = { ClusterName = var.ecs_cluster_name }
+  statistic = "Minimum"
+  period = 300
+  evaluation_periods = 1
+  threshold = 1
+  comparison_operator = "LessThanThreshold"
+  alarm_actions = [aws_sns_topic.alerts.arn]
+}
+
+# Dashboard can be added here using file()
+resource "aws_cloudwatch_dashboard" "dash" {
+  dashboard_name = "${var.project}-dashboard"
+  dashboard_body = file("${path.module}/../../dashboards/ecs_alb_dashboard.json")
+}
---
+++ modules/monitoring/variables.tf
@@ -0,0 +1,13 @@
+variable "project" { 
+    type = string 
+}
+variable "ecs_cluster_name" { 
+    type = string 
+}
+variable "sns_alert_email" { 
+    type = string 
+    default = "" 
+}
+variable "alb_name" { 
+    type = string 
+} # load balancer name (not arn)
