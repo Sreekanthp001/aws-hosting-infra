a23fe10a51bcc182524e35026650287d23679d8a
Sreekanthp001 <sreekanthpaleti1999@gmail.com>
2025-12-04 11:21:22 +0530

    infra creation

--- modules/client_site/variables.tf
+++ modules/client_site/variables.tf
@@ -1,18 +1,27 @@
-variable "domain" { 
-    type = string 
+variable "domain" {
+  type = string
 }
-variable "hosted_zone_id" { 
-    type = string 
+
+variable "hosted_zone_id" {
+  type = string
 }
-variable "create_ecs" { 
-    type = bool default = true 
+
+variable "create_ecs" {
+  type    = bool
+  default = true
 }
-variable "create_cloudfront" { 
-    type = bool default = false 
+
+variable "create_cloudfront" {
+  type    = bool
+  default = false
 }
-variable "enable_ses" { 
-    type = bool default = true 
+
+variable "enable_ses" {
+  type    = bool
+  default = true
 }
-variable "ecr_image" { 
-    type = string default = "" 
+
+variable "ecr_image" {
+  type    = string
+  default = ""
 } # e.g. ACCOUNT_ID.dkr.ecr...:sha
